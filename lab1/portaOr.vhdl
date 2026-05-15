0b7003443c28c2a41d64f0fa8bbe873a3bca5362
Lucila Costa <90205072+lucilagabriela@users.noreply.github.com>
2026-05-15 17:35:15 -0300

    Refactor PortaOr to use two inputs instead of four

--- lab1/portaOr.vhdl
+++ lab1/portaOr.vhdl
@@ -1,9 +1,12 @@
+-- Porta OR de duas entradas
 entity PortaOr is
-    port(input1, input2, input3, input4: in bit;
-          saida_or							  : out bit);
-    end PortaOr;
-    
-    architecture behav of PortaOr is
-    begin 
-        saida_or <= input1 or input2 or input3 or input4;
-    end architecture behav;
+    port(
+        input1, input2 : in  bit;
+        saida_or       : out bit
+    );
+end PortaOr;
+
+architecture behav of PortaOr is
+begin
+    saida_or <= input1 or input2;
+end architecture behav;
