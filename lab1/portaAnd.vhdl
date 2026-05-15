c1f913b42be8824beb05d9bb5fcd4d9a059640d3
Lucila Costa <90205072+lucilagabriela@users.noreply.github.com>
2026-05-15 17:34:38 -0300

    Refactor PortaAnd entity to yhree inputs

--- lab1/portaAnd.vhdl
+++ lab1/portaAnd.vhdl
@@ -1,10 +1,12 @@
+    -- Porta AND de duas entradas
 entity PortaAnd is
-    port(en1, en2, en3: in bit;
-          saida_and    : out bit);
-    end PortaAnd;
-    
-    architecture behav of PortaAnd is
-    begin
-        saida_and <= en1 and en2 and en3;
-        
-    end architecture behav;
+    port(
+        en1, en2  : in  bit;
+        saida_and : out bit
+    );
+end PortaAnd;
+
+architecture behav of PortaAnd is
+begin
+    saida_and <= en1 and en2;
+end architecture behav;
